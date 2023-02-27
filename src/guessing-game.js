90ca610fe81fc9cc546f116830b9d1dfa45c7c96
AaLin-Git <aalin.box.it@gmail.com>
2023-02-27 17:13:36 +0100

    feat: solve game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.result = Math.round(this.max - (this.max - this.min) / 2);
+    //console.log(this.result);
+    return this.result;
+  }
 
-    guess() {
+  lower() {
+    this.min = this.min;
+    this.max = this.result;
+    //console.log(this.min);
+    //console.log(this.max)
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.result;
+    this.max = this.max;
+    //console.log(this.min);
+  }
 }
 
 module.exports = GuessingGame;
